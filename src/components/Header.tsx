35ea87dbb72d007ea8dc27d21f26574a8055d854
Markus Bürgler <markus.buergler@me.com>
2022-04-04 16:41:59 +0200

    Fix paths

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,9 @@ const Header = () => {
   return (
     <>
       <div>
-        <Polaroid
-          src="/public/assets/images/header01.jpg"
-          caption={'Vroni&Markus'}
-        />
-        <Polaroid src="/public/assets/images/header02.jpg" caption={'♥'} />
-        <Polaroid
-          src="/public/assets/images/header03.jpg"
-          caption={'24.09.2022'}
-        />
+        <Polaroid src="/assets/images/header01.jpg" caption={'Vroni&Markus'} />
+        <Polaroid src="/assets/images/header02.jpg" caption={'♥'} />
+        <Polaroid src="/assets/images/header03.jpg" caption={'24.09.2022'} />
       </div>
       <h1 className="pb-5 text-center highlight-header">
         Zomm&apos; is anfoch schiana

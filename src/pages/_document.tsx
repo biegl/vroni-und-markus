4eebe59442f168d6833f882a0bfb6131b1447d78
Markus Bürgler <markus.buergler@me.com>
2022-10-27 11:32:38 +0200

    chore: add header to allow for google photo integration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <meta name="referrer" content="no-referrer" />
+        </Head>
         <body>
           <Main />
           <NextScript />
